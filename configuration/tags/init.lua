7420152196f5a17397280233fc1d76170f91e14c
jewel109 <jewelrana176517@gmail.com>
2022-10-28 09:25:51 +0600

    configuration/tags/init.lua is changed

--- configuration/tags/init.lua
+++ configuration/tags/init.lua
@@ -51,7 +51,18 @@ local tags = {
 awful.layout.layouts = {
   awful.layout.suit.tile,
   awful.layout.suit.max,
-  awful.layout.suit.floating
+  awful.layout.suit.floating,
+
+    awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile.top,
+    awful.layout.suit.fair,
+    awful.layout.suit.fair.horizontal,
+    awful.layout.suit.spiral,
+    awful.layout.suit.spiral.dwindle,
+    awful.layout.suit.max,
+    awful.layout.suit.max.fullscreen,
+    awful.layout.suit.magnifier,
+    awful.layout.suit.corner.nw,
 }
 
 awful.screen.connect_for_each_screen(
